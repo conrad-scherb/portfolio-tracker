598119b208f0b3c01bef02b8232406abd7dbae74
Conrad Scherb <csch666@aucklanduni.ac.nz>
2021-09-12 11:04:26 +1200

    Add get delayed stock price to API

--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -1,11 +1,23 @@
 import Vapor
 
+struct StockResponse: Content {
+    var delayedPrice: Double
+}
+
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return "Server is running"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("stock", "delayedprice", ":symbol") { req -> EventLoopFuture<String> in
+        let symbol = req.parameters.get("symbol")!
+        let client = req.client
+        let stockRequest = client.get("https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=\(iexCloudToken)")
+        
+        return stockRequest.flatMapThrowing { res in
+            try res.content.decode(StockResponse.self)
+        }.map { json in
+            return String(json.delayedPrice)
+        }
     }
 }
